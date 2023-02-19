4c938cb45ba12f224994571bf1c222dc965f0fb7
Joseph Ojoko <entewshi@gmail.com>
2023-02-19 06:23:18 +0100

    create nav menu component

---
+++ src/components/NavMenu.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import type { NavItems } from "@/types/nav";
+
+withDefaults(defineProps<{ items: NavItems }>(), { items: () => [] });
+</script>
+
+<template>
+  <v-list density="compact" nav>
+    <v-list-item
+      v-for="item in items"
+      :key="item.value"
+      :prepend-icon="item.icon"
+      :title="item.title"
+      :to="item.to"
+      :value="item.value"
+      color="primary"
+    />
+  </v-list>
+</template>
